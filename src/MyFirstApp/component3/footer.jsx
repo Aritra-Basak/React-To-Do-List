22bd3d2dcdefa0e6da68b9d9228c130e83c1510d
Aritra-Basak <basakaritra10@gmail.com>
2024-05-12 20:54:12 +0530

    Modified footer

--- src/MyFirstApp/component3/footer.jsx
+++ src/MyFirstApp/component3/footer.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 export default function footer() {
   return (
-    <div className='foot-container py-3'>
+    <div className="foot-container py-3">
       <div className="text-center">
-        <p><strong>Copyright &copy; MyToDoList.com</strong></p>
+        <p>
+          <strong>Copyright &copy; Aritra Basak(Dev)</strong>
+        </p>
       </div>
-      
     </div>
-  )
+  );
 }
